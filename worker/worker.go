73d6e3d65a90a343c769f7b4513297be79d7f93a
cohekoma <dinhhoangbaochan@gmail.com>
2026-03-26 21:46:28 +0700

    skeleton for worker

---
+++ worker/worker.go
@@ -0,0 +1,32 @@
+package worker
+
+import (
+	"fmt"
+
+	"github.com/cohekoma/project-sopranos/task"
+	"github.com/golang-collections/collections/queue"
+	"github.com/google/uuid"
+)
+
+type Worker struct {
+	Name      string
+	Queue     queue.Queue
+	Db        map[uuid.UUID]*task.Task
+	TaskCount int
+}
+
+func (w *Worker) CollectStats() {
+	fmt.Println("Worker begins to collect stats")
+}
+
+func (w *Worker) RunTask() {
+	fmt.Println("Run a task")
+}
+
+func (w *Worker) StartTask() {
+	fmt.Println("Starting task")
+}
+
+func (w *Worker) StopTask() {
+	fmt.Println("Stopping task")
+}
